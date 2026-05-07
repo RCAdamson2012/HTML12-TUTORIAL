26c1bdf6e783424a35622dbad1a7c5e564efca08
Robert Craig Adamson <rcadamson2@gmail.com>
2026-05-07 13:33:58 -0400

    12.1 Tutorial Commit

--- bc_outline.js
+++ bc_outline.js
@@ -27,3 +27,44 @@
 
 */
 
+/* Generate an outline based on h1 through h6 headings in the
+source document */
+
+window.addEventListener("load", makeOutline);
+
+function makeOutline() {
+   // Location of the document outline
+   var outline = document.getElementById("outline");
+
+   // Source document for the outline
+   var source = document.getElementById("doc");
+
+   var mainHeading = document.createElement("h1");
+   var outlineList = document.createElement("ol");
+   var headingText = document.createTextNode("Outline");
+
+   mainHeading.appendChild(headingText);
+   outline.appendChild(mainHeading);
+   outline.appendChild(outlineList);
+
+   createList(source, outlineList);
+}
+
+function createList(source, outlineList) {
+   // Headings for the outline
+var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+
+/* Loop through all of the child nodes of source
+article until no child nodes are left */
+
+   for (var n = source.firstChild; n !== null; n = n.nextSibling) {
+      // Examine only article headings
+   var headLevel = headings.indexOf(n.nodeName);
+   
+   if (headLevel !== -1) {
+      var listElem = document.createElement("li");
+      listElem.innerHTML = n.firstChild.nodeValue;
+      outlineList.appendChild(listElem);
+}
+   }
+}
